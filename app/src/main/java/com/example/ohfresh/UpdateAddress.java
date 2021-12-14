b6d10266012f1848e786d91dfb246661a49efe38
btttrang05 <trangbtt19411@st.uel.edu.vn>
2021-12-14 19:36:51 +0700

    Update Address

---
+++ app/src/main/java/com/example/ohfresh/UpdateAddress.java
@@ -0,0 +1,14 @@
+package com.example.ohfresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_address);
+    }
+}
