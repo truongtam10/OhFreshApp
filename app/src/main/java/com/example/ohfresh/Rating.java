159c933023039117dc88e654d16f42aa53d123df
Nguyen Truong Tam <nguyentruongtam01@gmail.com>
2021-12-23 11:47:29 +0700

    Add Banner to drawable, fix butoon in Rating Screen

--- app/src/main/java/com/example/ohfresh/Rating.java
+++ app/src/main/java/com/example/ohfresh/Rating.java
@@ -1,13 +1,20 @@
 package com.example.ohfresh;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Rating extends AppCompatActivity {
 
     ImageView imvBack;
+    Button btnSendRating;
+    EditText edtRatingContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,10 @@ public class Rating extends AppCompatActivity {
     }
 
     private void linkViews(){
+
         imvBack = findViewById(R.id.imvBack);
+        btnSendRating = findViewById(R.id.btnSendRating);
+        edtRatingContent = findViewById(R.id.edtRatingContent);
     }
 
     private void addEvents(){
@@ -27,6 +37,20 @@ public class Rating extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btnSendRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (edtRatingContent.getText().toString().trim().isEmpty()){
+                    Toast.makeText(Rating.this, "Bạn chưa nhập đánh giá!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(Rating.this, "Đánh giá thành công", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Rating.this, HomePage.class);
+                startActivity(intent);
             }
         });
     }
